baa4903901f411f43f494e2f8215a466e6c3c17c
Micaela Casais <casaismicaela@gmail.com>
2021-03-25 16:40:49 -0300

    exercise 1 done

---
+++ Exercise 1/exercise1.js
@@ -0,0 +1,36 @@
+/********************************
+a- Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+*********************************/
+
+var firstNumber, secondNumber, sum;
+
+firstNumber=50;
+secondNumber= 30;
+sum= firstNumber + secondNumber;
+
+console.log(sum);
+
+/***********************************
+b- Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+***********************************/
+
+
+var firstName, surname, completeName;
+
+firstName="Micaela";
+surname="Casais"
+completeName=firstName + surname;
+
+console.log(completeName)
+
+/***********************************
+c- Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando
+el resultado la suma una 3er variable (utilizar length).
+*************************************/
+
+var cityOfBirth, currentCity, citiesNameLenght;
+
+cityOfBirth= "SanNicolás"
+currentCity= "Rosario"
+citiesNameLenght = cityOfBirth.length + currentCity.length;
+console.log(citiesNameLenght);
